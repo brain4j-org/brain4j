94bf2bc935493c79bc560a47019210ae9029e456
echo <xreflection@outlook.it>
2025-01-06 22:45:43 +0100

    Added an adapter for the Updater

---
+++ src/main/java/net/echo/brain4j/adapters/UpdaterAdapter.java
@@ -0,0 +1,34 @@
+package net.echo.brain4j.adapters;
+
+import com.google.gson.*;
+import net.echo.brain4j.training.updater.Updater;
+import net.echo.brain4j.training.updater.impl.BatchedUpdater;
+import net.echo.brain4j.training.updater.impl.NormalUpdater;
+import net.echo.brain4j.training.updater.impl.StochasticUpdater;
+
+import java.lang.reflect.Type;
+
+public class UpdaterAdapter implements JsonSerializer<Updater>, JsonDeserializer<Updater> {
+
+    @Override
+    public JsonElement serialize(Updater updater, Type type, JsonSerializationContext context) {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("type", updater.getClass().getSimpleName());
+
+        return object;
+    }
+
+    @Override
+    public Updater deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject object = jsonElement.getAsJsonObject();
+        String optimizerType = object.get("type").getAsString();
+
+        return switch (optimizerType) {
+            case "BatchedUpdater" -> new BatchedUpdater();
+            case "StochasticUpdater" -> new StochasticUpdater();
+            case "NormalUpdater" -> new NormalUpdater();
+            default -> throw new IllegalArgumentException("Unknown updater type: " + optimizerType);
+        };
+    }
+}
--- src/main/java/net/echo/brain4j/training/updater/Updater.java
+++ src/main/java/net/echo/brain4j/training/updater/Updater.java
@@ -1,10 +1,14 @@
 package net.echo.brain4j.training.updater;
 
+import com.google.gson.annotations.JsonAdapter;
+import net.echo.brain4j.adapters.OptimizerAdapter;
+import net.echo.brain4j.adapters.UpdaterAdapter;
 import net.echo.brain4j.layer.Layer;
 import net.echo.brain4j.structure.Synapse;
 
 import java.util.List;
 
+@JsonAdapter(UpdaterAdapter.class)
 public abstract class Updater {
 
     public void postInitialize() {
