b7b4817112961d129e4166221b9d277ceab52f12
echo <xreflection@outlook.it>
2025-01-07 14:37:00 +0100

    Small changes to Xor test for benchmarks

--- src/test/java/XorTest.java
+++ src/test/java/XorTest.java
@@ -5,8 +5,9 @@ import net.echo.brain4j.model.Model;
 import net.echo.brain4j.model.initialization.WeightInit;
 import net.echo.brain4j.training.data.DataRow;
 import net.echo.brain4j.training.data.DataSet;
+import net.echo.brain4j.training.optimizers.impl.Adam;
 import net.echo.brain4j.training.optimizers.impl.AdamW;
-import net.echo.brain4j.training.updater.impl.StochasticUpdater;
+import net.echo.brain4j.training.updater.impl.NormalUpdater;
 import net.echo.brain4j.utils.Vector;
 
 public class XorTest {
@@ -14,18 +15,19 @@ public class XorTest {
     public static void main(String[] args) {
         Model model = new Model(
                 new DenseLayer(2, Activations.LINEAR),
-                new DenseLayer(64, Activations.RELU),
-                new DenseLayer(64, Activations.RELU),
+                new DenseLayer(32, Activations.RELU),
+                new DenseLayer(32, Activations.RELU),
                 new DenseLayer(1, Activations.SIGMOID)
         );
 
-        model.setSeed(123);
         model.compile(
                 WeightInit.HE,
                 LossFunctions.BINARY_CROSS_ENTROPY,
-                new AdamW(0.01),
-                new StochasticUpdater()
+                new Adam(0.1),
+                new NormalUpdater()
         );
+
+        System.out.println(model.getStats());
 
         DataRow first = new DataRow(Vector.of(0, 0), Vector.of(0));
         DataRow second = new DataRow(Vector.of(0, 1), Vector.of(1));
@@ -41,17 +43,19 @@ public class XorTest {
     private static void trainForBenchmark(Model model, DataSet data) {
         double total = 0.0;
 
-        for (int i = 0; i < 5000; i++) {
-            long start = System.nanoTime();
-            model.fit(data);
+        long start = System.nanoTime();
 
-            total += (System.nanoTime() - start) / 1e6;
+        for (int i = 0; i < 5000; i++) {
+            model.fit(data);
         }
 
-        double error = model.evaluate(data);
-        double mean = total / 5000;
+        long end = System.nanoTime();
+        double took = (end - start) / 1e6;
 
-        System.out.println("Completed 5000 epoches in " + total + " ms with error: " + error + " and an average of " + mean + " ms per epoch");
+        double error = model.evaluate(data);
+        double mean = took / 5000;
+
+        System.out.println("Completed 5000 epoches in " + took + " ms with error: " + error + " and an average of " + mean + " ms per epoch");
     }
 
     private static void trainTillError(Model model, DataSet data) {
