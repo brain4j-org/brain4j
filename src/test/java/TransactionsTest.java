17a6036e9ec31278aaceea9900d7199751d6f5d7
xEcho1337 <xreflection@outlook.it>
2025-01-27 20:17:27 +0100

    Extended TransactionsTest

--- src/test/java/TransactionsTest.java
+++ src/test/java/TransactionsTest.java
@@ -6,67 +6,49 @@ import net.echo.brain4j.model.initialization.WeightInit;
 import net.echo.brain4j.training.data.DataRow;
 import net.echo.brain4j.training.data.DataSet;
 import net.echo.brain4j.training.optimizers.impl.AdamW;
+import net.echo.brain4j.training.techniques.SmartTrainer;
+import net.echo.brain4j.training.techniques.TrainListener;
 import net.echo.brain4j.training.updater.impl.NormalUpdater;
 import net.echo.brain4j.utils.MLUtils;
 import net.echo.brain4j.utils.Vector;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class TransactionsTest {
 
-    private static final int INPUT_DIMENSION = 20;
+    private static final int INPUT_DIMENSION = 30;
 
     public static void main(String[] args) {
         Model model = getModel();
 
-        DataRow karhu = getKarhu();
-        DataRow polar = getPolar();
-        DataRow grim = getGrim();
+        DataSet dataSet = new DataSet(getKarhu(), getPolar(), getGrim(), getVulcan(), getMatrix(), getIntave());
+        dataSet.partition(1);
 
-        System.out.println(karhu);
-        System.out.println(polar);
-        System.out.println(grim);
+        SmartTrainer trainer = new SmartTrainer(1, 10_000);
 
-        DataSet training = new DataSet(karhu, polar, grim);
-        training.partition(1);
+        trainer.addListener(new TestListener());
+        trainer.start(model, dataSet, 0.01, 0.0001);
 
-        int i = 0;
-        double error = Double.MAX_VALUE;
-
-        do {
-            i++;
-
-            model.fit(training);
-
-            if (i % 10000 == 0) {
-                error = model.evaluate(training);
-                System.out.println("Epoch #" + i + " has error " + error);
-            }
-        } while (error > 0.01);
-
-        for (DataRow row : training) {
+        for (DataRow row : dataSet) {
             Vector output = model.predict(row.inputs());
 
             AntiCheat predicted = MLUtils.findBestMatch(output, AntiCheat.class);
             AntiCheat actual = MLUtils.findBestMatch(row.outputs(), AntiCheat.class);
 
-            System.out.println("-------------------------");
-            System.out.println("Inputs: " + row.inputs());
-            System.out.println("Output: " + row.outputs());
-            System.out.println("Prediction: " + output);
-            System.out.println("Actual Anticheat: " + actual);
-            System.out.println("Predicted Anticheat: " + predicted);
-            System.out.println();
+            System.out.println("Anticheat (Prediction, Actual): " + predicted + ", " + actual);
+            System.out.println("Distribution: " + output.toString("%.2f"));
         }
     }
 
     public static Model getModel() {
         Model model = new Model(
                 new DenseLayer(INPUT_DIMENSION, Activations.LINEAR),
-                new DenseLayer(8, Activations.SIGMOID),
-                new DenseLayer(8, Activations.SIGMOID),
-                new DenseLayer(3, Activations.SOFTMAX)
+                new DenseLayer(32, Activations.SIGMOID),
+                new DenseLayer(32, Activations.SIGMOID),
+                new DenseLayer(6, Activations.SOFTMAX)
         );
 
-        model.compile(WeightInit.UNIFORM_XAVIER, LossFunctions.CATEGORICAL_CROSS_ENTROPY, new AdamW(0.001), new NormalUpdater());
+        model.compile(WeightInit.UNIFORM_XAVIER, LossFunctions.CROSS_ENTROPY, new AdamW(0.001), new NormalUpdater());
 
         return model;
     }
@@ -78,17 +60,19 @@ public class TransactionsTest {
             vector.set(i, -i - 3000);
         }
 
-        return new DataRow(vector, Vector.of(1, 0, 0));
+        return new DataRow(vector, Vector.of(1, 0, 0, 0, 0, 0));
     }
 
     public static DataRow getPolar() {
         Vector vector = new Vector(INPUT_DIMENSION);
 
-        for (int i = 0; i < INPUT_DIMENSION; i++) {
+        vector.set(0, -ThreadLocalRandom.current().nextInt(2000));
+
+        for (int i = 1; i < INPUT_DIMENSION; i++) {
             vector.set(i, -i - 100);
         }
 
-        return new DataRow(vector, Vector.of(0, 1, 0));
+        return new DataRow(vector, Vector.of(0, 1, 0, 0, 0, 0));
     }
 
     public static DataRow getGrim() {
@@ -98,12 +82,52 @@ public class TransactionsTest {
             vector.set(i, -i);
         }
 
-        return new DataRow(vector, Vector.of(0, 0, 1));
+        return new DataRow(vector, Vector.of(0, 0, 1, 0, 0, 0));
+    }
+
+    public static DataRow getVulcan() {
+        Vector vector = new Vector(INPUT_DIMENSION);
+
+        for (int i = 0; i < INPUT_DIMENSION; i++) {
+            vector.set(i, -23767 + i);
+        }
+
+        return new DataRow(vector, Vector.of(0, 0, 0, 1, 0, 0));
+    }
+
+    public static DataRow getMatrix() {
+        Vector vector = new Vector(INPUT_DIMENSION);
+
+        for (int i = 0; i < INPUT_DIMENSION; i++) {
+            vector.set(i, i + 100);
+        }
+
+        return new DataRow(vector, Vector.of(0, 0, 0, 0, 1, 0));
+    }
+
+    public static DataRow getIntave() {
+        Vector vector = new Vector(INPUT_DIMENSION);
+
+        for (int i = 0; i < INPUT_DIMENSION; i++) {
+            vector.set(i, -((i + 1) % 16));
+        }
+
+        return new DataRow(vector, Vector.of(0, 0, 0, 0, 0, 1));
     }
 
     enum AntiCheat {
         KARHU,
         POLAR,
-        GRIM
+        GRIM,
+        VULCAN,
+        MATRIX,
+        INTAVE
+    }
+
+    static class TestListener extends TrainListener {
+        @Override
+        public void onEvaluated(DataSet dataSet, int epoch, double loss) {
+            System.out.println("Epoch " + epoch + " has loss: " + loss);
+        }
     }
 }
