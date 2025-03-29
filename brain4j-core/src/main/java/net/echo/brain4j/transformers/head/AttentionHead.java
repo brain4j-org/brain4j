ecd387c7e07cbd82df6bb86cc151a6e54a12e90b
Adversing <artomixgamer@gmail.com>
2025-03-29 01:52:18 +0100

    feat(transformers): implement caching mechanism in AttentionHead and MaskedAttentionHead for improved efficiency

--- brain4j-core/src/main/java/net/echo/brain4j/transformers/head/AttentionHead.java
+++ brain4j-core/src/main/java/net/echo/brain4j/transformers/head/AttentionHead.java
@@ -3,7 +3,10 @@ package net.echo.brain4j.transformers.head;
 import net.echo.brain4j.model.initialization.WeightInitializer;
 import net.echo.math4j.math.tensor.Tensor;
 import net.echo.math4j.math.tensor.TensorFactory;
+import net.echo.math4j.math.tensor.index.Range;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AttentionHead {
@@ -14,6 +17,10 @@ public class AttentionHead {
     protected final Tensor queryWeightsTensor;
     protected final Tensor keyWeightsTensor;
     protected final Tensor valueWeightsTensor;
+    
+    protected List<Tensor> keyCache;
+    protected List<Tensor> valueCache;
+    protected boolean useCache;
 
     public AttentionHead(int inputDimension, int headDimension) {
         this.inputDimension = inputDimension;
@@ -22,6 +29,10 @@ public class AttentionHead {
         this.queryWeightsTensor = TensorFactory.matrix(inputDimension, headDimension);
         this.keyWeightsTensor = TensorFactory.matrix(inputDimension, headDimension);
         this.valueWeightsTensor = TensorFactory.matrix(inputDimension, headDimension);
+        
+        this.keyCache = new ArrayList<>();
+        this.valueCache = new ArrayList<>();
+        this.useCache = true;
     }
 
     public void compile(Random random, WeightInitializer initializer) {
@@ -40,10 +51,66 @@ public class AttentionHead {
         return 3 * inputDimension * headDimension;
     }
 
+    public void setUseCache(boolean useCache) {
+        this.useCache = useCache;
+    }
+
+    public boolean isUsingCache() {
+        return useCache;
+    }
+
+    public void clearCache() {
+        keyCache.clear();
+        valueCache.clear();
+    }
+
     public Tensor attend(Tensor input) {
         Tensor Q = input.matmul(queryWeightsTensor);
-        Tensor K = input.matmul(keyWeightsTensor);
-        Tensor V = input.matmul(valueWeightsTensor);
+
+        int seqLength = input.shape()[0];
+        int cachedLength = keyCache.size();
+
+        Tensor K, V;
+        if (isUsingCache() && cachedLength > 0) {
+            if (seqLength > cachedLength) {
+                Range range = new Range(cachedLength, seqLength);
+                Tensor newInput = input.slice(range);
+
+                Tensor newK = newInput.matmul(keyWeightsTensor);
+                Tensor newV = newInput.matmul(valueWeightsTensor);
+
+                List<Tensor> newKTokens = TensorFactory.toList(newK);
+                List<Tensor> newVTokens = TensorFactory.toList(newV);
+
+                keyCache.addAll(newKTokens);
+                valueCache.addAll(newVTokens);
+            }
+
+            K = TensorFactory.zeros(seqLength, headDimension);
+            V = TensorFactory.zeros(seqLength, headDimension);
+            
+            int elementsToProcess = Math.min(seqLength, keyCache.size());
+            
+            for (int i = 0; i < elementsToProcess; i++) {
+                Tensor kToken = keyCache.get(i);
+                Tensor vToken = valueCache.get(i);
+                
+                for (int j = 0; j < headDimension; j++) {
+                    K.set(kToken.get(0, j), i, j);
+                    V.set(vToken.get(0, j), i, j);
+                }
+            }
+        } else {
+            K = input.matmul(keyWeightsTensor);
+            V = input.matmul(valueWeightsTensor);
+
+            clearCache();
+            List<Tensor> kTokens = TensorFactory.toList(K);
+            List<Tensor> vTokens = TensorFactory.toList(V);
+
+            keyCache.addAll(kTokens);
+            valueCache.addAll(vTokens);
+        }
 
         double normalizer = Math.sqrt(headDimension);
 
--- brain4j-core/src/main/java/net/echo/brain4j/transformers/head/MaskedAttentionHead.java
+++ brain4j-core/src/main/java/net/echo/brain4j/transformers/head/MaskedAttentionHead.java
@@ -2,6 +2,9 @@ package net.echo.brain4j.transformers.head;
 
 import net.echo.math4j.math.tensor.Tensor;
 import net.echo.math4j.math.tensor.TensorFactory;
+import net.echo.math4j.math.tensor.index.Range;
+
+import java.util.List;
 
 public class MaskedAttentionHead extends AttentionHead {
 
@@ -12,8 +15,51 @@ public class MaskedAttentionHead extends AttentionHead {
     @Override
     public Tensor attend(Tensor input) {
         Tensor Q = input.matmul(queryWeightsTensor);
-        Tensor K = input.matmul(keyWeightsTensor);
-        Tensor V = input.matmul(valueWeightsTensor);
+        
+        int seqLength = input.shape()[0];
+        int cachedLength = keyCache.size();
+        
+        Tensor K, V;
+        if (isUsingCache() && cachedLength > 0) {
+            if (seqLength > cachedLength) {
+                Range range = new Range(cachedLength, seqLength);
+                Tensor newInput = input.slice(range);
+                
+                Tensor newK = newInput.matmul(keyWeightsTensor);
+                Tensor newV = newInput.matmul(valueWeightsTensor);
+                
+                List<Tensor> newKTokens = TensorFactory.toList(newK);
+                List<Tensor> newVTokens = TensorFactory.toList(newV);
+                
+                keyCache.addAll(newKTokens);
+                valueCache.addAll(newVTokens);
+            }
+            
+            K = TensorFactory.zeros(seqLength, headDimension);
+            V = TensorFactory.zeros(seqLength, headDimension);
+            
+            int elementsToProcess = Math.min(seqLength, keyCache.size());
+            
+            for (int i = 0; i < elementsToProcess; i++) {
+                Tensor kToken = keyCache.get(i);
+                Tensor vToken = valueCache.get(i);
+                
+                for (int j = 0; j < headDimension; j++) {
+                    K.set(kToken.get(0, j), i, j);
+                    V.set(vToken.get(0, j), i, j);
+                }
+            }
+        } else {
+            K = input.matmul(keyWeightsTensor);
+            V = input.matmul(valueWeightsTensor);
+            
+            clearCache();
+            List<Tensor> kTokens = TensorFactory.toList(K);
+            List<Tensor> vTokens = TensorFactory.toList(V);
+            
+            keyCache.addAll(kTokens);
+            valueCache.addAll(vTokens);
+        }
 
         double normalizer = Math.sqrt(headDimension);
 
