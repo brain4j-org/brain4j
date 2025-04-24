e037c37e5d32f9117a3c3df96de9f6be6987fb4b
echo <xreflection@outlook.it>
2025-04-25 01:53:19 +0200

    added a big data test

--- brain4j-core/src/test/java/BatchExample.java
+++ brain4j-core/src/test/java/BatchExample.java
@@ -6,7 +6,7 @@ public class BatchExample {
 
     public static void main(String[] args) {
         int batchSize = 8192 * 5;
-        int samples = 307200;
+        int samples = 327680;
 
         int batches = samples / batchSize;
 
@@ -14,17 +14,19 @@ public class BatchExample {
         System.out.println("Batches: " + batches);
         Brain4J.useGPUIfAvailable();
 
-        Tensor a = Tensors.matrix(batchSize, 13);
+        Tensor a = Tensors.random(batchSize, 13);
 
-        Tensor b = Tensors.matrix(13, 256);
-        Tensor c = Tensors.matrix(256, 256);
-        Tensor d = Tensors.matrix(256, 256);
-        Tensor e = Tensors.matrix(256, 1);
+        Tensor b = Tensors.random(13, 256);
+        Tensor c = Tensors.random(256, 256);
+        Tensor d = Tensors.random(256, 256);
+        Tensor e = Tensors.random(256, 1);
 
         long start = System.nanoTime();
 
         for (int i = 0; i < batches; i++) {
-            a.matmul(b).matmul(c).matmul(d).matmul(e);
+            for (int j = 0; j < 3; j++) {
+                a.matmul(b).matmul(c).matmul(d).matmul(e);
+            }
         }
 
         long end = System.nanoTime();
---
+++ brain4j-core/src/test/java/BigDataExample.java
@@ -0,0 +1,65 @@
+import net.echo.brain4j.Brain4J;
+import net.echo.brain4j.layer.impl.DenseLayer;
+import net.echo.brain4j.loss.Loss;
+import net.echo.brain4j.model.Model;
+import net.echo.brain4j.model.impl.Sequential;
+import net.echo.brain4j.training.optimizer.impl.Adam;
+import net.echo.math.activation.Activations;
+import net.echo.math.data.ListDataSource;
+import net.echo.math.data.Sample;
+import net.echo.math.tensor.Tensor;
+import net.echo.math.tensor.Tensors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BigDataExample {
+
+    public static void main(String[] args) {
+        new BigDataExample().start();
+    }
+
+    public void start() {
+        Brain4J.useGPUIfAvailable();
+        Model model = new Sequential(
+                new DenseLayer(13, Activations.LINEAR),
+                new DenseLayer(256, Activations.RELU),
+                new DenseLayer(256, Activations.RELU),
+                new DenseLayer(256, Activations.RELU),
+                new DenseLayer(1, Activations.SIGMOID)
+        );
+
+        model.compile(Loss.BINARY_CROSS_ENTROPY, new Adam(0.01));
+
+        System.out.println(model.summary());
+
+        ListDataSource source = getDataSet();
+
+        Tensor packet = source.nextBatch().first();
+
+        System.out.println("Shape: " + Arrays.toString(packet.shape()));
+        System.out.println("Size: " + packet.elements());
+
+        source.reset();
+
+        long start = System.nanoTime();
+        model.fit(source);
+        long end = System.nanoTime();
+
+        System.out.println("Took: " + (end - start) / 1e6 + " ms");
+    }
+
+    public ListDataSource getDataSet() {
+        List<Sample> samples = new ArrayList<>();
+
+        for (int i = 0; i < 327680; i++) {
+            Tensor input = Tensors.random(13);
+            Tensor output = Tensors.random(1);
+
+            samples.add(new Sample(input, output));
+        }
+
+        return new ListDataSource(samples, 8);
+    }
+}
