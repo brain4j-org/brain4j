dca5ac849e80ef67bf92070edb8344ac384e8687
echo <xreflection@outlook.it>
2025-06-01 23:40:08 +0200

    feat: implemented positional encoding layer

---
+++ brain4j-core/src/main/java/org/brain4j/core/layer/impl/transformer/PosEncodeLayer.java
@@ -0,0 +1,68 @@
+package org.brain4j.core.layer.impl.transformer;
+
+import org.brain4j.core.layer.ForwardContext;
+import org.brain4j.core.layer.Layer;
+import org.brain4j.math.tensor.Tensor;
+import org.brain4j.math.tensor.Tensors;
+
+import java.util.Arrays;
+
+public class PosEncodeLayer extends Layer {
+
+    @Override
+    public Tensor forward(ForwardContext context) {
+        Tensor input = context.input();
+        // [batch_size, seq_length, dimension]
+        int[] shape = input.shape();
+
+        if (shape.length != 3) {
+            throw new IllegalArgumentException(
+                    "Expected input shape [batch_size, seq_length, dimension], got: " + Arrays.toString(shape)
+            );
+        }
+
+        int batchSize = shape[0];
+        int seqLength = shape[1];
+        int dimension = shape[2];
+
+        Tensor result = input.clone();
+        float[] resultData = result.data();
+
+        for (int b = 0; b < batchSize; b++) {
+            int batchOffset = b * seqLength * dimension;
+
+            for (int i = 0; i < seqLength; i++) {
+                Tensor add = generate(i, dimension);
+
+                float[] data = add.data();
+                int offset = batchOffset + i * dimension;
+
+                for (int k = 0; k < dimension; k++) {
+                    resultData[offset + k] += data[k];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    public Tensor generate(int position, int embeddingDim) {
+        Tensor token = Tensors.zeros(embeddingDim);
+
+        for (int i = 0; i < embeddingDim; i++) {
+            double exponent = (2.0 * Math.floor(i / 2.0)) / embeddingDim;
+
+            double angle = position / Math.pow(10000, exponent);
+            double value = (i % 2 == 0) ? Math.sin(angle) : Math.cos(angle);
+
+            token.set(value, i);
+        }
+
+        return token.reshape(1, embeddingDim);
+    }
+}
