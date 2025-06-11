f6a19d1330062d4eb4aa370d4c05830c8479f698
Adversing <artomixgamer@gmail.com>
2025-06-12 00:00:07 +0200

    fix(GELUActivation): replace hardcoded PI with constant reference

--- brain4j-math/src/main/java/org/brain4j/math/activation/impl/GELUActivation.java
+++ brain4j-math/src/main/java/org/brain4j/math/activation/impl/GELUActivation.java
@@ -4,6 +4,8 @@ import org.brain4j.math.activation.Activation;
 import org.brain4j.math.weights.WeightInitialization;
 import org.brain4j.math.weights.impl.NormalXavierInit;
 
+import static org.brain4j.math.constants.Constants.PI;
+
 public class GELUActivation implements Activation {
 
     @Override
@@ -13,12 +15,12 @@ public class GELUActivation implements Activation {
 
     @Override
     public double activate(double input) {
-        return 0.5 * input * (1 + Math.tanh(Math.sqrt(2 / Math.PI) * (input + 0.044715 * Math.pow(input, 3))));
+        return 0.5 * input * (1 + Math.tanh(Math.sqrt(2 / PI) * (input + 0.044715 * Math.pow(input, 3))));
     }
 
     @Override
     public double getDerivative(double input) {
-        double tanhTerm = Math.tanh(Math.sqrt(2 / Math.PI) * (input + 0.044715 * Math.pow(input, 3)));
-        return 0.5 * (1 + tanhTerm) + 0.5 * input * (1 - Math.pow(tanhTerm, 2)) * Math.sqrt(2 / Math.PI) * (1 + 3 * 0.044715 * Math.pow(input, 2));
+        double tanhTerm = Math.tanh(Math.sqrt(2 / PI) * (input + 0.044715 * Math.pow(input, 3)));
+        return 0.5 * (1 + tanhTerm) + 0.5 * input * (1 - Math.pow(tanhTerm, 2)) * Math.sqrt(2 / PI) * (1 + 3 * 0.044715 * Math.pow(input, 2));
     }
 }
