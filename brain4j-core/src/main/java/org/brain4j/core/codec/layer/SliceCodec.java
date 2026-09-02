5ca4785128f660a574a8d042cb032000aa53e6f6
xEcho1337 <123984252+xEcho1337@users.noreply.github.com>
2026-09-02 12:15:46 +0200

    feat(codec): implement `SliceLayer` serialization and deserialization functions

--- brain4j-core/src/main/java/org/brain4j/core/codec/layer/SliceCodec.java
+++ brain4j-core/src/main/java/org/brain4j/core/codec/layer/SliceCodec.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.brain4j.core.codec.Codec;
 import org.brain4j.core.layer.impl.utility.SliceLayer;
+import org.brain4j.math.commons.Commons;
 import org.brain4j.math.commons.Range;
 
 public class SliceCodec implements Codec<SliceLayer> {
@@ -21,12 +22,34 @@ public class SliceCodec implements Codec<SliceLayer> {
 
     @Override
     public void write(SliceLayer layer, ObjectNode out) {
-        // TODO
+        ArrayNode shape = out.putArray("ranges");
+        for (Range range : layer.ranges()) {
+            ArrayNode node = shape.arrayNode();
+            node.add(range.start());
+            node.add(range.end());
+            node.add(range.step());
+            shape.add(node);
+        }
     }
 
     @Override
     public SliceLayer parse(JsonNode in) {
-        // TODO
-        return null;
+        JsonNode jsonRanges = in.get("ranges");
+
+        if (jsonRanges == null || !jsonRanges.isArray()) {
+            throw Commons.illegalArgument("Ranges must be an array");
+        }
+
+        Range[] ranges = new Range[jsonRanges.size()];
+
+        for (int i = 0; i < jsonRanges.size(); i++) {
+            JsonNode node = jsonRanges.get(i);
+            int start = node.get(0).intValue();
+            int end = node.get(1).intValue();
+            int step = node.get(3).intValue();
+            ranges[i] = new Range(start, end, step);
+        }
+
+        return new SliceLayer(ranges);
     }
 }
