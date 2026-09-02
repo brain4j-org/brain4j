8bf26a49b1191cd551b9896296b20f4c0ac916f0
xEcho1337 <123984252+xEcho1337@users.noreply.github.com>
2026-09-02 12:45:33 +0200

    refactor(codec): centralize Transformer config serialization/deserialization logic in `TransformerCodec`

--- brain4j-core/src/main/java/org/brain4j/core/codec/layer/transformer/TransformerCodec.java
+++ brain4j-core/src/main/java/org/brain4j/core/codec/layer/transformer/TransformerCodec.java
@@ -2,7 +2,7 @@ package org.brain4j.core.codec.layer.transformer;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.brain4j.core.codec.Codec;
+import org.brain4j.core.codec.JsonCodec;
 import org.brain4j.core.importing.io.LayerIO;
 import org.brain4j.core.layer.Layer;
 import org.brain4j.core.layer.impl.NormLayer;
@@ -16,9 +16,36 @@ import java.util.function.Supplier;
 
 import static org.brain4j.core.importing.format.impl.BrainFormat.MAPPER;
 
-public interface TransformerCodec<T extends Transformer<?>> extends Codec<T> {
+public interface TransformerCodec<T extends Transformer<?>> extends JsonCodec<T> {
+
+    @Override
+    default void write(T value, ObjectNode out) {
+        Transformer.Config config = value.config();
+        out.put("num_heads", config.heads());
+        out.put("embedding_dim", config.embedDim());
+        out.put("projection_dim", config.projDim());
+        out.put("dropout", config.dropout());
+        out.put("gating", config.gating());
+
+        writeActivation(config.activation(), out);
+        writeNorm(config, out);
+    }
+
+    default Transformer.Config readConfig(JsonNode in) {
+        int heads = in.get("num_heads").asInt();
+        int dim = in.get("embedding_dim").asInt();
+        double dropout = in.get("dropout").asDouble();
+        int projDim = in.get("projection_dim").asInt();
+        boolean gating = in.get("gating").asBoolean();
+
+        Activation activation = parseActivation(in.get("activation"));
+        Supplier<Layer> normSupplier = parseNormSupplier(in.get("norm"));
+
+        return new Transformer.Config(dim, projDim, heads, dropout, gating, activation, normSupplier);
+    }
+
     default void writeActivation(Activation activation, ObjectNode out) {
-        Codec<Activation> codec = LayerIO.ACTIVATION_CODECS.get(activation.getClass());
+        JsonCodec<Activation> codec = (JsonCodec<Activation>) LayerIO.ACTIVATION_CODECS.get(activation.getClass());
 
         if (codec == null) {
             throw Commons.illegalArgument("Unknown activation type: %s", activation.getClass().getName());
@@ -38,9 +65,9 @@ public interface TransformerCodec<T extends Transformer<?>> extends Codec<T> {
         out.set("activation", node);
     }
 
-    default  void writeNorm(Transformer.Config config, ObjectNode out) {
+    default void writeNorm(Transformer.Config config, ObjectNode out) {
         Layer normInstance = config.normSupplier().get();
-        Codec<? extends Layer> codec = LayerIO.LAYER_CODECS.get(normInstance.getClass());
+        JsonCodec<? extends Layer> codec = (JsonCodec<? extends Layer>) LayerIO.LAYER_CODECS.get(normInstance.getClass());
         out.put("norm", codec.type());
     }
 
@@ -50,7 +77,7 @@ public interface TransformerCodec<T extends Transformer<?>> extends Codec<T> {
         }
 
         if (node.isTextual()) {
-            Codec<Activation> codec = LayerIO.ACTIVATION_CODECS.get(node.asText());
+            JsonCodec<Activation> codec = (JsonCodec<Activation>) LayerIO.ACTIVATION_CODECS.get(node.asText());
 
             if (codec == null) {
                 throw Commons.illegalArgument("Unknown activation type: %s", node.asText());
@@ -61,7 +88,7 @@ public interface TransformerCodec<T extends Transformer<?>> extends Codec<T> {
 
         String type = node.get("type").asText();
         JsonNode config = node.has("config") ? node.get("config") : MAPPER.createObjectNode();
-        Codec<Activation> codec = LayerIO.ACTIVATION_CODECS.get(type);
+        JsonCodec<Activation> codec = (JsonCodec<Activation>) LayerIO.ACTIVATION_CODECS.get(type);
 
         if (codec == null) {
             throw Commons.illegalArgument("Unknown activation type: %s", type);
--- brain4j-core/src/main/java/org/brain4j/core/codec/layer/transformer/TransformerDecoderCodec.java
+++ brain4j-core/src/main/java/org/brain4j/core/codec/layer/transformer/TransformerDecoderCodec.java
@@ -1,12 +1,7 @@
 package org.brain4j.core.codec.layer.transformer;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.brain4j.core.layer.Layer;
 import org.brain4j.core.layer.impl.transformer.Transformer;
-import org.brain4j.math.activation.Activation;
-
-import java.util.function.Supplier;
 
 public class TransformerDecoderCodec implements TransformerCodec<Transformer.Decoder> {
 
@@ -21,30 +16,7 @@ public class TransformerDecoderCodec implements TransformerCodec<Transformer.Dec
     }
 
     @Override
-    public void write(Transformer.Decoder layer, ObjectNode out) {
-        Transformer.Config config = layer.config();
-        out.put("num_heads", config.heads());
-        out.put("embedding_dim", config.embedDim());
-        out.put("projection_dim", config.projDim());
-        out.put("dropout", config.dropout());
-        out.put("gating", config.gating());
-
-        writeActivation(config.activation(), out);
-        writeNorm(config, out);
-    }
-
-    @Override
     public Transformer.Decoder parse(JsonNode in) {
-        int heads = in.get("num_heads").asInt();
-        int dim = in.get("embedding_dim").asInt();
-        double dropout = in.get("dropout").asDouble();
-        int projDim = in.get("projection_dim").asInt();
-        boolean gating = in.get("gating").asBoolean();
-
-        Activation activation = parseActivation(in.get("activation"));
-        Supplier<Layer> normSupplier = parseNormSupplier(in.get("norm"));
-
-        Transformer.Config config = new Transformer.Config(dim, projDim, heads, dropout, gating, activation, normSupplier);
-        return new Transformer.Decoder(config);
+        return new Transformer.Decoder(readConfig(in));
     }
 }
